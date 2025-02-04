8fe03a46a4b9d2b987ca6491ddb861abc43a1269
anmol <anmol333111@gmail.com>
2025-02-05 01:58:06 +0530

    Remove redundant response structure from getWatchHistory function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -457,12 +457,6 @@ const getWatchHistory = asyncHandler(async(req, res) => {
 })
 
 
-    return res.status(200)
-    .json(
-        new ApiResponse(200 , user[0].watchHistory , "Watch History fetched recieved")
-    )
-})
-
 
 
 export {
